00736feafe4d1a49716b6576f082d8819de32781
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2017-09-08 18:31:31 -0700

    Update temp_sensor.m

--- temp_sensor.m
+++ temp_sensor.m
@@ -1,43 +1,66 @@
 %% Using MATLAB to read serial output from an Arduino in real time
 
-%% Clear variables, Command line, Figures and Any Existing Serial Ports
+%% MATLAB Coding Inital Commands
 
-clc;
-clear;
-close all;
-delete(instrfindall);               % Clears any existing serial ports
+clc;                        % Clears the command line                      
+clear;                      % Erases all variables in the workspace
+close all;                  % Closes all open plot windows
+delete(instrfindall);       % Clears any existing serial ports
 
 %% Open The Serial Port To Connect To The Arduino
-% Change 'COM4' to whichever port the arduino is connected to
+% Check the serial port that the Arduino is connected to by:
+% Going to the Control Panel --> Hardware and Sound --> Devices and Printers
+% and right click: FT231X USB UART, select Properties and then look under
+% Hardware Tab
 
-arduino = serial('/dev/cu.usbserial-DN02SRFE','BaudRate',9600);
-fopen(arduino);
+% Change 'COM4' to whichever port the arduino is connected to.
+arduino = serial('COM4','BaudRate',9600);    % Creates a serial connection through COM4
+fopen(arduino);                              % Opens the serial to transmission
 
 %% Set-Up and Create The Animated Figure
 
-figure
-h = animatedline;
-ax = gca;              % gca - get current axis. Allows axis to be modified.
-ax.YGrid = 'on';       % Adds a grid on the plot.
-ax.YLim = [0 200];     % This is the serial output range
+figure(1)                % Assigns to the first plot window
+h = animatedline;        % Creates an animated plot line based on moving data output
+ax = gca;                % gca - get current axis. Allows axis to be modified
+ax.YGrid = 'on';         % Adds a grid to the plot
+ax.YLim = [0 200];       % This is the serial output range, so the y-axis range
 
-x = linspace(1,500,500);           % x can be changed to change the number of data points 
-startTime = datetime('now');       % Saves the current time
+Num_data = input('Enter the desired number of data points: '); % User-defined value for total data points plotted
+x = linspace(1,Num_data,Num_data);                       % x can be changed to alter the number of data points 
+startTime = datetime('now');                             % Saves the current time
 
 %% Collect Serial Data From The Arduino
 
-serial_data = zeros(500,1);             % Initializes data variable matrix at 0
+serial_data = zeros(500,1);                      % Initializes data variable matrix at 0 so for loop begins
 for i = 1:length(x)
-    serial_read = fscanf(arduino,'%f');          % Reads to serial output
-    serial_data(i) = fscanf(arduino,'%f');       % Saves the serial output
+    serial_read = fscanf(arduino,'%f');          % Reads to serial output as text
+    serial_data(i) = fscanf(arduino,'%f');       % Saves the serial output as text
     
-    t =  datetime('now') - startTime;            % Updates the time for current data point
+    t = datetime('now') - startTime;             % Updates the time for current data point
     addpoints(h,datenum(t),serial_read)          % Adds the data point to the figure
     
-    ax.XLim = datenum([t-seconds(15) t]);        % Updates the axes for new points
+    ax.XLim = datenum([t-seconds(15),t]);        % Updates the axes for new points
     datetick('x','keeplimits')                   % Updates tick marks
     drawnow                                      % Draws in the data point
 end
+
+%% Plots a Nice Graph
+
+figure(2);                              % Assigns to the second plot window
+t2 = linspace(1,15,500);                % Creates new variable for time that looks better for graphing
+Z = plot(t2, serial_data);              % Plots the serial_data from above vs the new time variable
+% Additions to the graph for reference and identification
+xlabel('Time (Seconds)');               % x-axis label
+ylabel('Temperature (Fahrenheit)');     % y-axis label
+title('Temperature vs. Time');          % Inserts a title to the plot
+legend('Temperature');                  % Adds a legend to the plot 
+ylim([0 200]);                          % Defines the axis limits
+grid;                                   % Turns on grid
+
+%% Close The Arduino Serial Object
+fclose(arduino);                    % Closes the serial channel
+clear(arduino);
+delete arduino;
 
 %% Plots a Nice Graph
 
